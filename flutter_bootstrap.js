d5d591fff731dfe44a7af7f49b4e83fe5591f9ef
Birinaite <121639842+birinaite@users.noreply.github.com>
2025-12-09 17:05:03 -0300

    another

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3143521117"
+    serviceWorkerVersion: "3640574630"
   }
 });
