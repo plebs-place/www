a67e66427f8a35ec7ad981e571241f83bce6d296
Birinaite <121639842+birinaite@users.noreply.github.com>
2025-12-09 18:15:23 -0300

    fix audio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2957017872"
+    serviceWorkerVersion: "2405383385"
   }
 });
